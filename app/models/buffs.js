9ecaf1c6458e9b2264c5664a5afed4c2039e5b84
Haris Skiadas <cskiadas@gmail.com>
2014-06-27 18:50:46 -0400

    Base buffs setup

---
+++ app/models/buffs.js
@@ -0,0 +1,68 @@
+(function(define) { 'use strict';
+define(function(require) {
+
+   var mixin = require('../utils/mixin');
+   var hasteMultiply = require('../utils/math').hasteMultiply;
+   var Spec = require('./spec');
+
+   var defaultSettings = {
+      baseIntellect       : 293,
+      gearIntellect       : 874,
+      weaponSpellpower    : 313,
+      baseSpirit          : 220,
+      gearSpirit          : 0,
+      criticalRating      : 76,
+      hasteRating         : 234,
+      masteryRating       : 254,
+      buffStats           : true, // 5%
+      buffSpellpower      : true, // 10%
+      buffCritical        : true, // 5%
+      buffMastery         : true, // 458???
+      buffHaste           : true, // 5%  (and multistrike?)
+      level               : 90
+   }
+
+   var convs = {
+      critical : 1 / 23,     // critRating to crit
+      haste    : 1 / 16,
+      mastery  : 1 / 23     // This is before taking into account spec-mastery coefficient
+   }
+
+   function Buffs(settings) {
+      return mixin({}, defaultSettings, settings);
+   }
+
+   Buffs.prototype = {
+      // This computes effects as if those base stats will not be further modified
+      // Specs that affect base stats must first clone the object, then adjust the stats
+      // and then have it compute with the adjusted stats.
+      compute = function() {
+         var intellect  = (this.baseIntellect + this.gearIntellect) * 1.05 *
+                           (this.buffStats ? 1.05 : 1);
+         var spellpower = (intellect + this.weaponSpellpower) * (this.buffSpellpower ? 1.1 : 1);
+         var spirit     = this.baseSpirit + this.gearSpirit;
+         var critical   = this.critRating * convs.critical + 6 + (this.buffCritical ? 5 : 0);
+         var mastery    = (this.masteryRating + (this.buffMastery ? 118 : 0)) * convs.masteryRating + 8;
+         var haste      = this.hasteRating * convs.haste;
+         var adjustedHaste = (this.buffHaste ? hasteMultiply(haste, 5) : haste);
+
+         return {
+            intellect      : intellect,
+            spellpower     : spellpower,
+            spirit         : spirit,
+            critical       : critical,
+            mastery        : mastery,
+            haste          : adjustedHaste,
+            masteryPercent : mastery * 1.25  // Needs to be overwritten by spec
+         }
+      },
+      clone = function() {
+         return new Buffs(this);
+      }
+   }
+
+   return Buffs;
+
+});
+
+}(typeof define == 'function' && define.amd ? define : function(factory) { module.exports = factory(require); }));
