f679eb56c42bb46e7e0bf718bea4777bfab7fbbd
Haris Skiadas <cskiadas@gmail.com>
2014-06-29 16:10:42 -0400

    First pass at the pally spec

--- app/models/spec/pally.js
+++ app/models/spec/pally.js
@@ -1,21 +1,64 @@
 (function(define) { 'use strict';
 define(function(require) {
    
-   var percentIncrease = require('../../utils/math').percentIncrease
+   var math = require('../../utils/math');
+   var percentIncrease = math.percentIncrease;
+   var hasteMultiply = math.hasteMultiply;
+   var masteryFactor = 1.25;
+
+
+   // This is meant to replace healWithMast
+   var sealInsightHeal = function(that) { return this.baseHeal(that) * 1.05; }
+   var beaconedHeal = function(percent) {
+      var percent = percent || 0.5; // 50% default
+      return function(that) {
+         return this.baseHealWithMast(that) +
+                (that.stats.beacon ? this.heal(that) * percent : 0);
+      }
+   }
+
+
    return {
       settings : {
          // Used to set up spec-specific choices/talents
       },
       amendRatings : {
-         criticalRating : percentIncrease(5),
+         criticalRating : percentIncrease(5),   // Critical Strike Atunement
          baseIntellect  : percentIncrease(5),
          gearIntellect  : percentIncrease(5)
       },
       amendStats : function(stats) {
          // Used for post-processing of the stats. Will often do nothing to the stats
+         stats.masteryPercent = stats.mastery * masteryFactor;
+         stats.haste = hasteMultiply(stats.haste, 1.1);   // Pallies get 10% passive haste
          return stats;
       },
       spellEffects : {
+         'FlashLight' : {
+            healWithMast : beaconedHeal(0.5),
+            heal : sealInsightHeal
+         },
+         'HolyShock' : {
+            healWithMast : beaconedHeal(0.5),
+            heal : sealInsightHeal
+         },
+         'LightOfDawn' : {
+            healWithMast : beaconedHeal(0.5),
+            heal : sealInsightHeal
+         },
+         'HolyLight' : {
+            healWithMast : beaconedHeal(0.5),
+            heal : sealInsightHeal
+         },
+         'HolyRadiance' : {
+            healWithMast : beaconedHeal(0.5),
+            heal : sealInsightHeal,
+            ntargets : function(that) { return 1 + 5/2; } // To account for half-healing on nonmain
+         },
+         'WordOfGlory' : {
+            healWithMast : beaconedHeal(0.5),
+            heal : sealInsightHeal
+         }
       }
    }
 
--- app/models/spell/pally.js
+++ app/models/spell/pally.js
@@ -1,7 +1,77 @@
 (function(define) { 'use strict';
 define(function(require) {
 
-   return [ ]
+   return [
+      {
+         nick    : 'FlashLight',
+         name    : 'Flash of Light',
+         mana    : 0.23 / 5,
+         ct      : 1.5,
+         coef    : 1.85304 * 1.25,
+         img     : 'link',
+         link    : 19750,
+         instant : false,
+         aoe     : false
+      },
+      {
+         nick    : 'HolyShock',
+         name    : 'Holy Shock',
+         mana    : 0.071 / 5,
+         cd      : 6,
+         ct      : 1.5,
+         coef    : 1.22162 * 1.25,
+         img     : 'link',
+         link    : 20473,
+         instant : true,
+         aoe     : false
+      },
+      {
+         nick     : 'LightOfDawn',
+         name     : 'Light of Dawn',
+         mana     : 0,
+         ct       : 1.5,
+         coef     : 0.735 * 1.5,
+         ntargets : 6,
+         img      : 'link',
+         link     : 85222,
+         instant  : true,
+         aoe      : true
+      },
+      {
+         nick     : 'HolyLight',
+         name     : 'Holy Light',
+         mana     : 0.115 / 5,
+         ct       : 2.5,
+         coef     : 1.853 * 1.25,
+         img      : 'link',
+         link     : 82326,
+         instant  : false,
+         aoe      : false
+      },
+      {
+         nick     : 'HolyRadiance',
+         name     : 'Holy Radiance',
+         mana     : 0.44 / 5,
+         ct       : 2.5,
+         coef     : 1.5132 * 1.25,
+         ntargets : 6,
+         img      : 'link',
+         link     : 82327,
+         instant  : false,
+         aoe      : true
+      },
+      {
+         nick     : 'WordOfGlory',
+         name     : 'Word of Glory',
+         mana     : 0,
+         ct       : 1.5,
+         coef     : 0.882 * 1.5 * 3,
+         img      : 'link',
+         link     : 136494,
+         instant  : true,
+         aoe      : false
+      }
+   ]
 
 });
 
