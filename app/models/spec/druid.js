9de5513c18d6bf0931ad7d7eb251415f3200c4ee
Haris Skiadas <cskiadas@gmail.com>
2014-06-28 20:07:01 -0400

    First pass at druid spell implementation

--- app/models/spec/druid.js
+++ app/models/spec/druid.js
@@ -16,8 +16,31 @@ define(function(require) {
          return stats;
       },
       spellEffects : {
+         // Living seed only on base heal, and does not benefit further from mastery
+         "HealingTouch" : {
+            critHeal : function(that) { return this.critBasicHeal(that) * 1.3; }
+         },
          "Regrowth" : {
-            critChance : function(that) { return this.baseCritChance(that) + 0.6; }
+            basicHeal : function(that) { return 79 + this.params.coef * that.stats.spellpower; },
+            critChance : function(that) { return this.baseCritChance(that) + 0.6; },
+            healWithMast : function(that) { return this.hotWithMast(that) + this.basicHealWithMast(that); },
+            critHeal : function(that) {
+               return this.critHot(that) + this.critBasicHeal(that) * 1.3;
+            },
+            avgHeal : function(that) {
+               var C = this.baseCritChance(that);
+               return this.baseAvgHeal(that) -   // This is computed with the increased crit chance
+                      this.critHot(that) * 0.6; // Need to remove the extra criting of dots
+            }
+         },
+         "Lifebloom" : {
+            heal : function(that) { return this.hot(that) + this.basicHeal(that); }
+         },
+         "Rejuv" : {
+            heal : function(that) { return this.hot(that) + this.basicHeal(that); }
+         },
+         "Tranq" : {
+            nticks : function(that) { return this.params.nticks; }
          }
       }
    }
--- app/models/spell/druid.js
+++ app/models/spell/druid.js
@@ -1,73 +1,118 @@
 (function(define) { 'use strict';
 define(function(require) {
-
+   // Almost all coefs come with a 1.1 factor to account for Naturalist 10% increased healing
+   // All mana costs divided by 5, as they are based on the reduced mana pool that non-spec shamans have
    return [
       {
          nick    : 'HealingTouch',
          name    : 'Healing Touch',
-         mana    : 0.115 / 5, // Percent of mana pool
+         mana    : 0.115 / 5,
          ct      : 2.5,
-         coef    : 3.31916 * 1.2,   // Improved Healing
+         coef    : 3.31916 * 1.1,
          img     : 'link',
-         link    : 5185,           // id of wow-head's link
+         link    : 5185,
          instant : false,
          aoe     : false
       },
       {
          nick    : 'Swiftmend',
          name    : 'Swiftmend',
-         mana    : 0.242 / 5, // Percent of mana pool
+         mana    : 0.242 / 5,
          cd      : 15,
          ct      : 1.5,
-         coef    : 4.285 * 1.2,    // Improved Healing
+         coef    : 4.285 * 1.1,
          img     : 'link',
-         link    : 18562,           // id of wow-head's link
+         link    : 18562,
          instant : true,
          aoe     : false
       },
       {
          nick     : 'Lifebloom',
          name     : 'Lifebloom',
-         mana     : 0.05 / 5, // Percent of mana pool
+         mana     : 0.05 / 5,
          ct       : 1.5,
-         coefdot  : 0.36729 * 1.1,
+         coefhot  : 0.36729 * 1.1,
          coef     : 4.518,
          nticks   : 15,
          timetick : 1,
          img      : 'link',
-         link     : 33763,           // id of wow-head's link
+         link     : 33763,
          instant  : true,
          aoe      : false
       },
       {
          nick     : 'Regrowth',
          name     : 'Regrowth',
-         mana     : 0.23 / 5, // Percent of mana pool
+         mana     : 0.23 / 5,
          ct       : 1.5,
-         coefdot  : 0.36729 * 1.1,
-         coef     : 4.518,
+         coefhot  : 0.546264 / 6 * 1.2,
+         coef     : 1.6524 * 1.2,
          nticks   : 6,
          timetick : 2,
          img      : 'link',
-         link     : 8936,           // id of wow-head's link
+         link     : 8936,
          instant  : false,
          aoe      : false
       },
       {
-         nick     : 'Regrowth',
-         name     : 'Regrowth',
-         mana     : 0.23 / 5, // Percent of mana pool
+         nick     : 'Rejuv',
+         name     : 'Rejuvenation',
+         mana     : 0.105 / 5,
          ct       : 1.5,
-         coefdot  : 0.71179,
+         coefhot  : 0.71179 * 1.1,
+         coef     : 0.71179 * 1.1,  // Used for the initial heal/tick
+         nticks   : 4,
+         timetick : 3,
+         img      : 'link',
+         link     : 774,
+         instant  : true,
+         aoe      : false
+      },
+      {
+         nick     : 'WildGrowth',
+         name     : 'Wild Growth',
+         mana     : 0.23 / 5,
+         cd       : 8,
+         ct       : 1.5,
+         coefhot  : 3.318/7 * 1.1,
+         nticks   : 7,
+         timetick : 1,
+         ntargets : 5,
+         img      : 'link',
+         link     : 48438,
+         instant  : true,
+         aoe      : true
+      },
+      {
+         nick     : 'Tranq',
+         name     : 'Tranquility',
+         mana     : 0.23 / 5,
+         cd       : 8 * 60,
+         ct       : 1.5,
+         coefhot  : 2.502 * 1.1,
          nticks   : 4,
          timetick : 2,
+         ntargets : 10,
          img      : 'link',
-         link     : 8936,           // id of wow-head's link
+         link     : 740,
          instant  : false,
-         aoe      : false
+         aoe      : true
+      },
+      {
+         nick     : 'Mushroom',
+         name     : 'Wild Mushroom',
+         mana     : 0.27 / 5,
+         ct       : 1.5,
+         coefhot  : 0.6209,
+         nticks   : 30 / 2,
+         timetick : 2,
+         ntargets : 3,
+         img      : 'link',
+         link     : 147349,
+         instant  : true,
+         aoe      : true
       }
    ]
-
 });
 
 }(typeof define == 'function' && define.amd ? define : function(factory) { module.exports = factory(require); }));
